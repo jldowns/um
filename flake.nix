649a50cc5438115fbbca8003bd69a325ea15726f
Justin D <git@justindowns.com>
2022-06-02 13:53:33 -0400

    added flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "um - create and maintain your own man pages";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      rec {
+
+        # Packages
+        packages = flake-utils.lib.flattenTree {
+          hello = pkgs.hello;
+          gitAndTools = pkgs.gitAndTools;
+        };
+        defaultPackage = packages.hello;
+
+        # Devshell
+        devShell.default = 
+          mkShell {
+            buildInputs = with pkgs; [ruby_3_0];
+          };
+
+        # Apps
+        apps.hello = flake-utils.lib.mkApp { drv = packages.hello; };
+        defaultApp = apps.hello;
+      }
+    );
+}
