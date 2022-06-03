525b3dc4644234ef6038b1e2a8b670b3975b1a66
Justin D <git@justindowns.com>
2022-06-02 20:34:25 -0400

    added gemspec, hopefully can factor it out in future

---
+++ gemset.nix
@@ -0,0 +1,22 @@
+{
+  kramdown = {
+    groups = ["default"];
+    platforms = [];
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "1n1c4jmrh5ig8iv1rw81s4mw4xsp4v97hvf8zkigv4hn5h542qjq";
+      type = "gem";
+    };
+    version = "1.17.0";
+  };
+  um = {
+    dependencies = ["kramdown"];
+    groups = ["default"];
+    platforms = [];
+    source = {
+      path = ./.;
+      type = "path";
+    };
+    version = "4.2.0";
+  };
+}
